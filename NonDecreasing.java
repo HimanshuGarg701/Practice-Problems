6e25a29c7e3ce16e5ce5b67eb6beb2ad09dcb0eb
Himanshu Garg <hgarg701@gmail.com>
2019-10-22 22:42:29 -0700

    /TODO Find the correct algorithm

---
+++ NonDecreasing.java
@@ -0,0 +1,17 @@
+
+package leetcode.practice;
+
+/**
+ *
+ * @author Himanshu Garg
+ */
+public class NonDecreasing {
+    public static void main(String[] args){
+        int arr [] = {1,3,4,5,3,2,6};
+        System.out.println("The fact about order of array is : " + isDecreasing(arr));
+    }
+    
+    public static boolean isDecreasing(int[] arr){
+        return false;
+    }
+}
