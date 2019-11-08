109efe8872defb331799024892593bf41fb6d222
Himanshu Garg <hgarg701@gmail.com>
2019-11-07 17:50:57 -0800

    Getting max profit by buying and selling stock

--- BuyAndSell.java
+++ BuyAndSell.java
@@ -26,30 +26,5 @@ public class BuyAndSell {
             }
         }
         return profit;
-// store maximum profit gained
-//		int profit = 0;
-//
-//		// initialize local minimum to first element's index
-//		int j = 0;
-//
-//		// start from second element
-//		for (int i = 1; i < price.length; i++)
-//		{
-//			// update local minimum if decreasing sequence is found
-//			if (price[i - 1] > price[i]) {
-//				j = i;
-//			}
-//
-//			// sell shares if current element is peak
-//			// i.e. (previous <= current > next)
-//			if (price[i - 1] <= price[i] &&
-//				(i + 1 == price.length || price[i] > price[i + 1]))
-//			{
-//				profit += (price[i] - price[j]);
-//				System.out.printf("Buy on day %d and sell on day %d\n", j + 1, i + 1);
-//			}
-//		}
-//
-//		return profit;
     }
 }
