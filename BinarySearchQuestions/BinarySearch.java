c25015ecc22cb20ab2877056c9d9d02185a185a7
Himanshu Garg <hgarg701@gmail.com>
2020-07-02 18:02:29 -0700

    Added Binary Search Questions

---
+++ BinarySearchQuestions/BinarySearch.java
@@ -0,0 +1,32 @@
+package BinarySearchQuestions;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        findNum(array, 5);
+        findNum(array, 11);
+    }
+
+    private static void findNum(int[] array, int num){
+        int start = 0;
+        int end = array.length-1;
+        int mid;
+        boolean found = false;
+        while(start<=end){
+            mid = start + (end - start)/2;
+            if(num < array[mid]){
+                start = mid+1;
+            }
+            else if(num > array[mid]){
+                end = mid-1;
+            }
+            else{
+                System.out.println("Number found: " + num);
+                found = true;
+                break;
+            }
+        }
+        if(!found)
+            System.out.println("Number not found : " + num);
+    }
+}
