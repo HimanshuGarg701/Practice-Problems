39d13f91c283ae64aa5629b8e012ac47a1dcd906
Himanshu Garg <hgarg701@gmail.com>
2020-07-03 14:45:15 -0700

    Added driver code in the driver class

--- Graph/Driver.java
+++ Graph/Driver.java
@@ -3,5 +3,12 @@ package Graph;
 public class Driver {
     public static void main(String[] args){
         GraphImplementation graph = new GraphImplementation();
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+
+        graph.addEdge(1,5);
+        graph.addEdge(2,3);
+        graph.addEdge(3,6);
     }
 }
