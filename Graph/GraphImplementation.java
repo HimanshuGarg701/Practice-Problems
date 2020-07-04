d77251dc3d85d001fc485df6e233fe9bef4ef4a1
Himanshu Garg <hgarg701@gmail.com>
2020-07-04 14:40:08 -0700

    Added addVertex and addEdge implementation

--- Graph/GraphImplementation.java
+++ Graph/GraphImplementation.java
@@ -6,10 +6,20 @@ public class GraphImplementation<T> {
     private HashMap<T, LinkedList<T>> map = new HashMap();
 
     public void addVertex(T vertex){
-
+        if(!map.containsKey(vertex)){
+            map.put(vertex, new LinkedList<T>());
+        }
     }
 
     public void addEdge(T begin, T end){
-        
+        if(!map.containsKey(begin)){
+            addVertex(begin);
+        }
+
+        if(!map.containsKey(end)){
+            addVertex(end);
+        }
+
+        map.get(begin).add(end);
     }
 }
