595fbd694a21e477e8bd3db4fda89e751d5803f9
Himanshu Garg <hgarg701@gmail.com>
2020-09-07 14:35:45 -0700

    K closest numbers

--- Heap/AllInOne.java
+++ Heap/AllInOne.java
@@ -1,11 +1,13 @@
 package Heap;
 
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class AllInOne {
     public static void main(String[] args){
         int[] array = {2,3,4,5,6,7,8,9,0,1};
         findKSmallest(array, 4);
+        closestKNumbers(array, 4, 10);
     }
 
     public static void findKSmallest(int[] arr, int k){
@@ -20,4 +22,24 @@ public class AllInOne {
 
         System.out.println(maxHeap.peek());
     }
+
+    public static void closestKNumbers(int[] arr, int k, int num){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: arr){
+            map.put(i, Math.abs(i-num));
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)-> map.get(b) - map.get(a));
+
+        for(int i: arr){
+            maxHeap.add(i);
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        System.out.print("Closest 4 numbers to 10: ");
+        while(maxHeap.size()!=0){
+            System.out.print("  " + maxHeap.poll() + "  ");
+        }
+    }
 }
+
