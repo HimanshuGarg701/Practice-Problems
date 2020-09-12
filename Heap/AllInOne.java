e8e3221fe53ebe09f112bff1a569e14bc355acdc
Himanshu Garg <hgarg701@gmail.com>
2020-09-12 16:07:54 -0700

    K closest to origin

--- Heap/AllInOne.java
+++ Heap/AllInOne.java
@@ -6,8 +6,10 @@ import java.util.PriorityQueue;
 public class AllInOne {
     public static void main(String[] args){
         int[] array = {2,3,4,5,6,7,8,9,0,1};
+        Integer[][] points = { {1,1},{0,1},{2,2},{0,2},{0,0} };
         findKSmallest(array, 4);
         closestKNumbers(array, 4, 10);
+        closestToOrigin(points, 2);
     }
 
     public static void findKSmallest(int[] arr, int k){
@@ -39,6 +41,26 @@ public class AllInOne {
         System.out.print("Closest 4 numbers to 10: ");
         while(maxHeap.size()!=0){
             System.out.print("  " + maxHeap.poll() + "  ");
+        }
+    }
+
+    public static void closestToOrigin(Integer[][] arr, int k){
+        HashMap<Integer[], Integer> map = new HashMap<>();
+        for(Integer[] i: arr){
+            map.put(i, i[0]*i[1]);
+        }
+
+        PriorityQueue<Integer[]> maxHeap = new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
+        for(Integer[] j : arr){
+            maxHeap.add(j);
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+
+        while(!maxHeap.isEmpty()){
+            Integer[] data = maxHeap.poll();
+            System.out.print("\n[ " + data[0] + " ], [ " + data[1] + " ]\n");
         }
     }
 }
