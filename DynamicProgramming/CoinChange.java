6731b3007f7ed2a66ea7ce4e44b59aaa976964f4
Himanshu Garg <hgarg701@gmail.com>
2020-10-20 19:03:22 -0700

    Minimum number of coins required to find given total

---
+++ DynamicProgramming/CoinChange.java
@@ -0,0 +1,38 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public static void main(String[] args){
+        int[] coins = {3,2,8};
+        int sum = 11;
+
+        int[][] matrix = new int[coins.length][sum+1];
+        for(int i=0; i<matrix[0].length; i++){
+            if(i%coins[0]==0){
+                matrix[0][i]= i/coins[0];
+            }else{
+                matrix[0][i] = Integer.MAX_VALUE-coins.length;
+            }
+        }
+        for(int i=0; i<matrix.length; i++){
+            matrix[i][0] = 0;
+        }
+        System.out.println("Minimum coins required: " + findMin(coins, sum, matrix));
+        for(int[] i: matrix){
+            System.out.println(Arrays.toString(i));
+        }
+    }
+
+    public static int findMin(int[] coins, int sum, int[][] matrix){
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                matrix[i][j] = matrix[i-1][j];
+                if(coins[i]<=j){
+                    matrix[i][j] = Math.min(matrix[i-1][j], 1+matrix[i][j-coins[i]]);
+                }
+            }
+        }
+        return matrix[matrix.length-1][matrix[0].length-1];
+    }
+}
