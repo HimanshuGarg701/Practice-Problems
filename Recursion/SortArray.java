d54c3c28d8a1b669dd939be3c7346d7c596b2e81
Himanshu Garg <hgarg701@gmail.com>
2020-11-09 19:28:31 -0800

    Sorting array using recursion

---
+++ Recursion/SortArray.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class SortArray {
+    public static void main(String[] args){
+        int[] array = {1,3,4,2,5};
+        sort(array);
+    }
+
+    private static void sort(int[] array){
+
+    }
+}
