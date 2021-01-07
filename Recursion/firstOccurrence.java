766c7dd0688b2edd54e9ece44bac5bda9ea1a5e5
Himanshu Garg <hgarg701@gmail.com>
2021-01-06 22:24:20 -0800

    Enhanced firstOccurrence using recursion

--- Recursion/firstOccurrence.java
+++ Recursion/firstOccurrence.java
@@ -3,19 +3,19 @@ package Recursion;
 public class firstOccurrence {
     public static void main(String[] args){
         int[] arr = {1,4,5,6,7,8,6,5,4};
-        findFirstOccurrence(arr, arr.length-1, 6, -1);
+        System.out.println("Element present at index: " + findFirstOccurrence(arr, 0, 6));
     }
 
-  public static void findFirstOccurrence(int[] arr, int n, int element, int index){
-        if(n==-1){
-            System.out.println("The index of element is: " + index);
-            return;
+  public static int findFirstOccurrence(int[] arr, int n, int element){
+        if(n==arr.length){
+            System.out.println("Not found");
+            return -1;
         }
-
         if(arr[n]==element){
-            index = n;
+            System.out.println("The index of element is: " + n);
+            return n;
         }
-        findFirstOccurrence(arr, n-1, element, index);
+        return findFirstOccurrence(arr, n+1, element);
   }
 
 
