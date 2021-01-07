d8785188a44a9b4496b05537dd5096f9272f7cd0
Himanshu Garg <hgarg701@gmail.com>
2021-01-06 16:49:22 -0800

    Enhanced power calculation for a number

---
+++ Recursion/PowerEnhanced.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class PowerEnhanced {
+    public static void main(String[] args){
+        System.out.println(powerEnhanced(2, 4));
+    }
+
+    public static int powerEnhanced(int n, int p){
+        if(p==0){
+            return 1;
+        }else{
+           
+            int subResult = powerEnhanced(n, p/2);
+            int result = subResult * subResult;
+            if(p%2!=0){
+                result = result * n;
+            }
+            return result;
+        }
+    }
+}
