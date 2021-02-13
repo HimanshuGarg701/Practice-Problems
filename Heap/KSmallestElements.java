24dbec9c0bcb20954c75012cba4cdf476918e0cd
Himanshu Garg <hgarg701@gmail.com>
2021-02-13 00:31:06 -0800

    fINDING K LARGEST ELEMENTSD IN THE ARRAY

--- Heap/KSmallestElements.java
+++ Heap/KSmallestElements.java
@@ -7,6 +7,7 @@ public class KSmallestElements {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,7,6,5,8,9};
         findKSmallest(arr, 3);
+        findKLargest(arr, 3);
     }
 
     private static void findKSmallest(int[] array, int k){
@@ -15,8 +16,22 @@ public class KSmallestElements {
             maxHeap.add(i);
         }
         int count = 0;
+        System.out.println("Printing k smallest elements");
         while(maxHeap.size()>0 && count<k){
             System.out.println(maxHeap.poll());
+            count++;
+        }
+    }
+
+    private static void findKLargest(int[] array, int k){
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        for(int i: array){
+            minHeap.add(i);
+        }
+        int count = 0;
+        System.out.println("\n\nPrinting k largest elements");
+        while(minHeap.size()>0 && count<k){
+            System.out.println(minHeap.poll());
             count++;
         }
     }
